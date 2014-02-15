2d6fa6f1a4b1e1c2edc029d102b6c4d6dc36f757
Jonathan Sisk <jonsisk@gmail.com>
2014-02-14 17:25:09 -0700

    Removed new themes, reworked meta-cat-tags
    Removed new themes, reworked meta-cat-tags

--- wp-content/themes/idahoednews/meta-cat-tags.php
+++ wp-content/themes/idahoednews/meta-cat-tags.php
@@ -1,8 +1,8 @@
 <div class="cat-chicklets">
 <?php
-if (in_category('superintendents')) {?><div class="cat-chicklet superintendents">super race</div><?};
-if (in_category('governor')) {?><div class="cat-chicklet governor" >governor</div><?};
-if (in_category('elections')) {?><div class="cat-chicklet elections">elections</div><?};
-if (in_category('briefs')) {?><div class="cat-chicklet briefs">briefs</div><?};
+if (in_category('superintendents')) {echo '<div class="cat-chicklet superintendents">super race</div>';};
+if (in_category('governor')) {echo '<div class="cat-chicklet governor" >governor</div>';};
+if (in_category('elections')) {echo '<div class="cat-chicklet elections">elections</div>';};
+if (in_category('briefs')) {echo '<div class="cat-chicklet briefs">briefs</div>';};
 ?>
 </div>
